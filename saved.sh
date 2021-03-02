039b334f7adcd2c50bb9a37043e88c1d50566e6b
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-02 19:56:57 +0600

    Update saved.sh

--- saved.sh
+++ saved.sh
@@ -1,7 +1,7 @@
 clear
 toilet -f mono12 -F gay "SDMH"
 echo ""
-echo -e "\e[96m----------------- SDMH-Hacking-TV 2021--------\e[0m";
+echo -e "\e[96m----------------- SDMH-Hacking-TV 2021-------------\e[0m";
 echo -e "\e[95m--------------- Facebook-SDMH-Hacking-TV ----------\e[0m";
 echo -e "\e[93m----------- FB Phis and Android Pattern Hack ------\e[0m";
 echo ""
