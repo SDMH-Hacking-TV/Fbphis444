95dc9fee471f370bbd4821e866fa7d72cc5a5d2e
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-02 18:55:55 +0600

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,9 +1,9 @@
 clear
-toilet -f mono12 -F gay "AIOP"
+toilet -f mono12 -F gay "SDMH"
 echo ""
 echo -e "\e[96m----------------- SDMH-Hacking-TV 2021--------\e[0m";
-echo -e "\e[95m----------- FB Phis and Android Pattern Hack ----------\e[0m";
-echo -e "\e[93m-------------- Facebook - SDMH-Hacking-TV ------\e[0m";
+echo -e "\e[95m--------------- Facebook-SDMH-Hacking-TV ----------\e[0m";
+echo -e "\e[93m----------- FB Phis and Android Pattern Hack ------\e[0m";
 echo ""
 echo ""
 cowsay -f ghostbusters CODED BY ISURUWA | lolcat -a
@@ -13,6 +13,6 @@ echo "FACEBOOK : https://www.facebook.com/group/SDMH.Hacking.TV | lolcat
 echo "GMAIL : isuruumayanga035@gmail.com" | lolcat
 echo ""
 read -p 'Press Enter To Continue 》' userinput1;
-bash aiop.sh
+bash fbphis.sh
 
 
