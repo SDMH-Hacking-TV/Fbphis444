e22791d140b241632b7658540c1cbaeaabe504df
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-02 19:16:19 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 echo -e "\e[93mUpdating ...\e[0m";cd $HOME
 rm -rf Fbphis444
-git clone https://github.com/SDMH-Hacking-TV/Fbphis444
+git clone https://github.com/SDMH-Hacking-TV/Fbphis444.git
 cd Fbphis444
 bash setup.sh
