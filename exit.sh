4b07c7cd42ef0051075f25cbd9ed8c04ae741349
SDMH-Hacking-TV <77563341+SDMH-Hacking-TV@users.noreply.github.com>
2021-03-02 19:59:31 +0600

    Update exit.sh

--- exit.sh
+++ exit.sh
@@ -1,7 +1,7 @@
 clear
 toilet -f mono12 -F gay "Fbphis444"
 echo ""
-echo -e "\e[96m----------------- SDMH-Hacking-TV 2021--------\e[0m";
+echo -e "\e[96m----------------- SDMH-Hacking-TV 2021-------------\e[0m";
 echo -e "\e[95m--------------- Facebook-SDMH-Hacking-TV ----------\e[0m";
 echo -e "\e[93m----------- FB Phis and Android Pattern Hack ------\e[0m";
 echo ""
